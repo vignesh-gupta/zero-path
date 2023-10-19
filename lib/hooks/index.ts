1d941c4dcb6652014de3b2f8f250e6fe0c25c0f2
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-10-19 10:47:14 +0530

    [fix] state name

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useLocalStorage } from "./use-local-storage";
 export { default as useDebounce } from "./use-debounce";
+export { default as useFetch } from "./use-fetch";
---
+++ lib/hooks/use-fetch.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState<null | unknown>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+      setData(json);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return { data, error, isLoading, fetchData };
+};
+
+export default useFetch;
