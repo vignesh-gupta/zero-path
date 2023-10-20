219e7c6ecd46990b147da52f1a33b450d11cd82d
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-10-20 22:34:40 +0530

    [updated] theme switch

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col bg-background min-h-screen">
       <Navbar />
-      <main className="h-full grow ">{children}</main>
+      <main className="h-full grow">{children}</main>
       <FooterSection />
     </div>
   );
