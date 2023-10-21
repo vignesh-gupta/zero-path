69a33cad0cdb55476d365d4c659ab4b14a15907d
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-10-21 22:40:47 +0530

    [added] nFormatter util

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function nFormatter(num: number, digits: number) {
+	const si = [
+		{ value: 1, symbol: "" },
+		{ value: 1E3, symbol: "k" },
+		{ value: 1E6, symbol: "M" },
+		{ value: 1E9, symbol: "G" },
+		{ value: 1E12, symbol: "T" },
+		{ value: 1E15, symbol: "P" },
+		{ value: 1E18, symbol: "E" }
+	];
+	const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+	let i;
+	for (i = si.length - 1; i > 0; i--) {
+		if (num >= si[i].value) {
+			break;
+		}
+	}
+	return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
