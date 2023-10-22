74090299f15dee23ceec398e7cb97f421cbc97f3
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-10-22 13:50:59 +0530

    [added] twitter announcement badge

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,18 +4,49 @@ import clsx from "clsx";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-import { Button } from "@/components/ui/button";
-import { DEPLOY_URL } from "@/lib/constants";
-import { poppins } from "@/lib/fonts";
+import { poppins, atkinsonHyperlegible } from "@/lib/fonts";
 import SocialButton from "@/components/shared/social-btn";
 
 const HeroSection = () => {
   return (
     <div className="z-10 w-full max-w-xl py-11 flex flex-col justify-center items-center">
+      <Link
+        rel="noopener noreferrer"
+        target="_blank"
+        href={
+          process.env.NEXT_PUBLIC_TWEET_LINK ||
+          "https://twitter.com/VighneshGupta9"
+        }
+      >
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className={clsx(
+            "flex p-4 py-2 border border-muted bg-black/80 dark:bg-white text-white dark:text-black rounded-full shadow-lg gap-2 mb-5 items-center",
+            atkinsonHyperlegible.className,
+          )}
+        >
+          <svg
+            width="15"
+            height="15"
+            className="text-white dark:text-black"
+            viewBox="0 0 1200 1227"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
+              fill="currentColor"
+            />
+          </svg>
+          Introducing Zero-Path
+        </motion.p>
+      </Link>
       <motion.h1
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
         className={clsx(
           "bg-gradient-to-br from-primary to-muted-foreground bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm [text-wrap:balance] md:text-5xl pb-2",
           poppins.className,
@@ -47,28 +78,6 @@ const HeroSection = () => {
         transition={{ duration: 0.5, delay: 0.4 }}
         className="mx-auto mt-6 flex items-center justify-center space-x-5 "
       >
-        {/* <Button
-          asChild
-          className="group gap-2 text-sm shadow-xl text-white hover:text-black hover:bg-white rounded-full hover:shadow-lg border hover:border-muted-foreground border-primary"
-        >
-          <Link href={DEPLOY_URL} target="_blank" rel="noopener noreferrer">
-            <svg
-              className="h-4 w-4 group-hover:text-black"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </svg>
-            <p>Deploy to Vercel</p>
-          </Link>
-        </Button> */}
         <SocialButton icon="vercel" />
         <SocialButton icon="github" />
       </motion.div>
